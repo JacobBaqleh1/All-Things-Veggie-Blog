84d436e476ad2a8363d6e21f4179fa8f8416fab2
JacobBaqleh1 <101436252+JacobBaqleh1@users.noreply.github.com>
2022-10-06 12:35:06 -0700

    2 files modified

--- src/snacks/homemade-granola-bars/Navbar.js
+++ src/snacks/homemade-granola-bars/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div >
       <nav >
         <div className="center-nav">
-        <h1 className="nav-title">Jacob's Food Blog</h1>
+        <h1 className="nav-title">Veggie Cooking</h1>
         <img></img>
         <div className="nav-links">
           <a href="">Newsletter</a>
